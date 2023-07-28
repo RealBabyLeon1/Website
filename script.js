e68efc27407f9675e719ca06c59519a03c102bd3
RealBabyLeon1 <139175459+RealBabyLeon1@users.noreply.github.com>
2023-07-28 11:53:13 -0400

    super real commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let leanPic = document.querySelector("#Lean")
+let myDiv = document.querySelector("div")
+let myButton = document.querySelector("button")
+let realDealDiv = document.querySelector("#theDiv")
+
+myButton.addEventListener("click", e=>{
+e.preventDefault()
+myDiv.classList.toggle("begone")
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
